9a60397355190269e2730cc4f332ddae032675f7
Ki Jong Byun <kjbyun0@gmail.com>
2023-06-28 00:52:17 -0500

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr) {
+    return arr.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(arr) {
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multVal) {
+    return (fare) => fare * multVal;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arr, cb) {
+    return cb(arr);
+}
